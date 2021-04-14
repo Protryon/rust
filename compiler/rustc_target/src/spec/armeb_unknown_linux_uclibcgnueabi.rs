6b36125401c7e86bc15838d7c7102bf5e62f459c
Protryon <max.bruce12@gmail.com>
2021-04-14 06:57:29 -0700

    update uclibc armeb support

--- compiler/rustc_target/src/spec/armeb_unknown_linux_uclibcgnueabi.rs
+++ compiler/rustc_target/src/spec/armeb_unknown_linux_uclibcgnueabi.rs
@@ -1,26 +1,23 @@
-use crate::spec::{LinkerFlavor, Target, TargetOptions, TargetResult};
+use crate::spec::{LinkerFlavor, Target, TargetOptions};
+use crate::abi::Endian;
 
-pub fn target() -> TargetResult {
-    let base = super::linux_base::opts();
-    Ok(Target {
+pub fn target() -> Target {
+    let base = super::linux_uclibc_base::opts();
+    Target {
         llvm_target: "armeb-unknown-linux-uclibcgnueabi".to_string(),
-        target_endian: "big".to_string(),
-        target_pointer_width: "32".to_string(),
-        target_c_int_width: "32".to_string(),
+        pointer_width: 32,
         data_layout: "E-m:e-p:32:32-Fi8-i64:64-v128:64:128-a:0:32-n32-S64".to_string(),
         arch: "arm".to_string(),
-        target_os: "linux".to_string(),
-        target_env: "uclibc".to_string(),
-        target_vendor: "unknown".to_string(),
-        linker_flavor: LinkerFlavor::Gcc,
 
         options: TargetOptions {
+            linker_flavor: LinkerFlavor::Gcc,
+            endian: Endian::Big,
             features: "+strict-align,+v6,+soft-float".to_string(),
             cpu: "generic".to_string(),
             max_atomic_width: Some(32),
             unsupported_abis: super::arm_base::unsupported_abis(),
-            target_mcount: "\u{1}__gnu_mcount_nc".to_string(),
+            mcount: "\u{1}__gnu_mcount_nc".to_string(),
             ..base
         },
-    })
+    }
 }
